c89034a9da890f918598949aa2b011ae27ec76a5
Maximilian Saerov <max.saerov@gmail.com>
2024-06-24 16:35:03 +0000

    added new file structure and more object classes

--- data_subset_extraction.py
+++ data_subset_extraction.py
@@ -1,49 +1,47 @@
 import os
+import pandas as pd
+import json
 import shutil
 
 # object classes: 
 # used models adm, biggan, stablediffusion
 # used object classes(ai_id, nature_id): - sports car(817, n04285008), samoyed(258, n02111889), peacock(84, n01806143), pizza(963, n07873807) and violin(889, n04536866)
 
-
 model_paths = ["adm", "biggan", "stablediffusion"]
-ai_prefixes = ["84_", "817_", "258_", "963_", "889_"]
-natural_prefixes = ["n04285008_", "n04536866", "n07873807", "n01806143", "n02111889"]
+ai_prefixes = {"084_":"peacock", "817_":"sportscar", "258_":"samoyed", "963_":"pizza", "889_":"violin"}
+natural_prefixes = {"n01806143":"peacock", "n04285008_":"sportscar", "n02111889":"samoyed", "n07873807":"pizza", "n04536866":"violin"}
 
-def extract_and_copy_ai_images(model_path, prefixes):
+def extract_and_copy_images(model_path, prefixes, type):
     base_path = os.path.join(os.getcwd(), model_path)
-    image_files = []
-    for prefix in prefixes:
+
+    for prefix in prefixes.keys():
+        image_files = []
         for root, dirs, files in os.walk(base_path):
             for file in files:
                 if file.startswith(prefix):
                     image_files.append(os.path.join(root, file))
 
-    destination_folder = os.path.join("subset_evaluation", model_path, "ai")
-    if not os.path.exists(destination_folder):
-            os.makedirs(destination_folder)
-    for file_path in image_files:
-        destination_path = os.path.join(destination_folder, os.path.basename(file_path))
-        if not os.path.exists(destination_path):
-            shutil.copy(file_path, destination_path)
+        destination_folder = os.path.join("subset_evaluation", model_path, f"{type}_{prefixes[prefix]}")
+        if not os.path.exists(destination_folder):
+                os.makedirs(destination_folder)
+        for file_path in image_files:
+            destination_path = os.path.join(destination_folder, os.path.basename(file_path))
+            if not os.path.exists(destination_path):
+                shutil.copy(file_path, destination_path)
 
-def extract_and_copy_natural_images(model_path, prefixes):
-    base_path = os.path.join(os.getcwd(), model_path)
-    image_files = []
-    for prefix in prefixes:
-        for root, dirs, files in os.walk(base_path):
-            for file in files:
-                if file.startswith(prefix):
-                    image_files.append(os.path.join(root, file))
+df = pd.read_csv("Selected_classes.csv")
 
-    destination_folder = os.path.join("subset_evaluation", model_path, "nature")
-    if not os.path.exists(destination_folder):
-            os.makedirs(destination_folder)
-    for file_path in image_files:
-        destination_path = os.path.join(destination_folder, os.path.basename(file_path))
-        if not os.path.exists(destination_path):
-            shutil.copy(file_path, destination_path)
+with open("imagenet_class_index.json", 'r') as file:
+    imgNet_classes = json.load(file)
+    for number in df["Number"]:
+        number_str = str(number)
+        name = imgNet_classes[number_str][1]
+        nature_prefix = imgNet_classes[number_str][0]
+        ai_prefix = number_str.zfill(3) +"_"
+        ai_prefixes[ai_prefix] = name
+        natural_prefixes[nature_prefix] = name
+
 
 for model_path in model_paths: 
-    extract_and_copy_ai_images(model_path, ai_prefixes)
-    extract_and_copy_natural_images(model_path, natural_prefixes)
+    extract_and_copy_images(model_path, ai_prefixes, type="ai")
+    extract_and_copy_images(model_path, natural_prefixes, type="nature")
