3325f276fa260eea053074530ad808b25ede12be
SumoPikmin <pikminzwiebel@gmail.com>
2024-07-17 13:33:08 +0200

    deleted ourVisDiff for folder name change

--- ourVisDiff/applications/LaMem/get_lamem_results.py
+++
@@ -1,7 +0,0 @@
-import os
-
-if __name__ == "main":
-    os.system(f"python main.py --config configs/lamem.yaml project=LaMem")
-    os.system(
-        f'python main.py --config configs/lamem.yaml project=LaMem data.group1="not memorable" data.group2="memorable"'
-    )
