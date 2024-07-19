70f37dd7df6d74bca444c82b449d44102228c4f6
Maximilian Saerov <max.saerov@gmail.com>
2024-07-19 15:10:56 +0000

    add global_vars, rm from gitignore

---
+++ ourVisDiff/serve/global_vars.py
@@ -0,0 +1,14 @@
+# LLM API
+VICUNA_URL = "http://localhost:8000/v1"
+LLM_CACHE_FILE = "cache/cache_llm"
+
+# VLM API
+LLAVA_CODE_PATH = "./LLaVA"
+BLIP_URL = "http://localhost:8082"
+LLAVA_URL = "http://localhost:8084"
+BLIP_FEATURE_URL = "http://localhost:8086"
+VLM_CACHE_FILE = "cache/cache_vlm"
+
+# CLIP API
+CLIP_URL = "http://localhost:8090"
+CLIP_CACHE_FILE = "cache/cache_clip"
